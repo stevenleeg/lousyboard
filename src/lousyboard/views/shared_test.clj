0d826b28a6ff7b22b41fb89e338832e042263ad6
Steve Gattuso <steve@stevegattuso.me>
2016-10-04 00:53:57 -0400

    removing test in a way wrong place

--- src/lousyboard/views/shared_test.clj
+++
@@ -1,8 +0,0 @@
-(ns lousyboard.views.shared-test
-  (:require [clojure.test :refer :all]
-            [lousyboard.views.shared :refer :all]))
-
-(deftest test-defview
-  (testing "does this right"
-    (is 1 2)))
-
