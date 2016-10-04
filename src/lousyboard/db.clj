ae033faaa240fee90f67f862865ad6290bfe0b72
Steve Gattuso <steve@stevegattuso.me>
2016-10-03 21:03:18 -0400

    Using environment variables for the db

--- src/lousyboard/db.clj
+++ src/lousyboard/db.clj
@@ -1,9 +1,12 @@
 (ns lousyboard.db
   (:require [korma.db :refer :all]
-            [ragtime.jdbc :as jdbc]))
+            [ragtime.jdbc :as jdbc]
+            [environ.core :refer [env]]))
 
-(def db-config (postgres {:db "lousy"
-                          :user "lousy"}))
+(def db-config (postgres {:host (env :db-host)
+                          :db (env :db-name)
+                          :user (env :db-user)
+                          :password (env :db-password)}))
 
 (def migration-config
   {:datastore (jdbc/sql-database db-config)
--- src/lousyboard/views/shared.clj
+++ src/lousyboard/views/shared.clj
@@ -1,6 +1,12 @@
 (ns lousyboard.views.shared
   (:require [hiccup.core :refer :all]
+            [hiccup.element :refer [link-to]]
             [hiccup.page :refer :all]))
+
+(def footer
+  [:div {:class "footer"}
+   "powered by " 
+   (link-to "https://github.com/stevenleeg/lousyboard" "lousyboard")])
 
 (defn layout 
   "Defines the default layout for views in the application"
@@ -10,4 +16,6 @@
          (include-css "/dist/application.css")
          (include-js "/app.js")]
         [:body
-         [:div {:class "container"} content]]))
+         [:div {:class "container"} 
+          content
+          footer]]))
